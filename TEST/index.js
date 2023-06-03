fd30f5edac5ad62dc629745ce579cde6d5e259e7
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-03 12:09:01 +0530

    creating writing and reading streams

--- TEST/index.js
+++ TEST/index.js
@@ -1,38 +1,48 @@
-// // import fs from "fs";
-// // import inquirer from "inquirer";
-
-// // fs.writeFile("hello.js",string,(err=>console.log(err)));
-
-// import readline from "readline"
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// let a;
-
-// rl.question('Enter something: ', (userInput) => {
-// a=userInput;
-// //   console.log('You entered: ' + userInput);
-//   rl.close();
-// });
-
-// console.log("Hello " + a);
-
+import fs from "fs";
 import inquirer from "inquirer";
+
+fs.writeFile("hello.js",string,(err=>console.log(err)));
+
+import readline from "readline"
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 let a;
 
-async function getInput(callback){
+rl.question('Enter something: ', (userInput) => {
+a=userInput;
+//   console.log('You entered: ' + userInput);
+  rl.close();
+});
+
+console.log("Hello " + a);
+
+import inquirer from "inquirer";
+// let a;
+
+async function saveguard(){
+    try{
+        progress=fs.readFileSync("hello2.js","utf-8",err=>console.log(err));
+    }catch{
+        (err=>console.log(err))
+    }
+}
+
+
+
+async function getName(){
     try{
    const answer = await  inquirer.prompt([
             {
                 type: 'input',
-                name: 'userInput',
+                name: 'userName',
                 message: 'Enter something: '
             }
         ])
-        a = answer.userInput
-        callback(a);
+        a =await answer.userName
+       await fs.writeFileSync("hello2.js",a,(err=>console.log("err!")))
 
      } catch{
             (error => {
@@ -41,11 +51,32 @@ async function getInput(callback){
     }
     }
 
-    getInput(a=>console.log(a))
+    async function getBranch(){
+        try{
+
+        const data = fs.readFile("hello2.js",(err)=>console.log(err))
+
+       const answer = await  inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'userBranch',
+                    message: 'Enter something: '
+                }
+            ])
+            a = await answer.userBranch
+            await fs.writeFileSync("hello2.js",a,(err=>console.log("err!")))
     
-   async function yo(a){
-        console.log(" hELLO  " + a);
+         } catch{
+                (error => {
+                console.error(error);
+            });
+        }
+        }
+    
 
-   }
 
-yo(a);
+    await getName();
+    await getBranch();
+    const data =fs.readFileSync("hello2.js",'utf-8',(err)=>console.log(err))
+    console.log(data + `\nhe\nllo
+    World!`)

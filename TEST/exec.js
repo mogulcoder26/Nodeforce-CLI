446c50943041a0b5b13db7b6f1657fb8bb2309d1
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-10 09:25:11 +0530

    executables running well

---
+++ TEST/exec.js
@@ -0,0 +1,10 @@
+
+import {exec} from "child_process";
+var yourscript = exec('sh bash.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
--- TEST/index.js
+++ TEST/index.js
@@ -1,82 +1,96 @@
 import fs from "fs";
 import inquirer from "inquirer";
-
-fs.writeFile("hello.js",string,(err=>console.log(err)));
-
 import readline from "readline"
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-let a;
+// fs.writeFile("hello.js",string,(err=>console.log(err)));
 
-rl.question('Enter something: ', (userInput) => {
-a=userInput;
-//   console.log('You entered: ' + userInput);
-  rl.close();
-});
 
-console.log("Hello " + a);
-
-import inquirer from "inquirer";
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 // let a;
 
-async function saveguard(){
-    try{
-        progress=fs.readFileSync("hello2.js","utf-8",err=>console.log(err));
-    }catch{
-        (err=>console.log(err))
-    }
-}
+// rl.question('Enter something: ', (userInput) => {
+// a=userInput;
+// //   console.log('You entered: ' + userInput);
+//   rl.close();
+// });
+
+// console.log("Hello " + a);
+
+// let a;
+
+// async function saveguard(){
+//     try{
+//         progress=fs.readFileSync("hello2.js","utf-8",err=>console.log(err));
+//     }catch{
+//         (err=>console.log(err))
+//     }
+// }
 
 
 
-async function getName(){
-    try{
-   const answer = await  inquirer.prompt([
-            {
-                type: 'input',
-                name: 'userName',
-                message: 'Enter something: '
-            }
-        ])
-        a =await answer.userName
-       await fs.writeFileSync("hello2.js",a,(err=>console.log("err!")))
+// async function getName(){
+//     try{
+//    const answer = await  inquirer.prompt([
+//             {
+//                 type: 'input',
+//                 name: 'userName',
+//                 message: 'Enter something: '
+//             }
+//         ])
+//         a =await answer.userName
+//        await fs.writeFileSync("hello2.js",a,(err=>console.log("err!")))
 
-     } catch{
-            (error => {
-            console.error(error);
-        });
-    }
-    }
+//      } catch{
+//             (error => {
+//             console.error(error);
+//         });
+//     }
+//     }
+let a,progress;
 
     async function getBranch(){
         try{
 
-        const data = fs.readFile("hello2.js",(err)=>console.log(err))
+        // const data = fs.readFile("hello2.js",(err)=>console.log(err))
 
        const answer = await  inquirer.prompt([
                 {
                     type: 'input',
                     name: 'userBranch',
-                    message: 'Enter something: '
+                    message: 'Enter '
                 }
             ])
+
             a = await answer.userBranch
-            await fs.writeFileSync("hello2.js",a,(err=>console.log("err!")))
+            await fs.writeFileSync("hello3.js",a,(err=>console.log("err!")))
+
+            progress=fs.readFileSync("hello3.js","utf-8",err=>console.log(err));
+
+            const answer2 = await  inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'usercock',
+                    message: 'Enter '
+                }
+            ])
+
+            a = await answer2.usercock
+            await fs.writeFileSync("hello3.js",progress + a,(err=>console.log("err!")))
+
     
-         } catch{
-                (error => {
+         } catch(error){
+           
                 console.error(error);
-            });
+            };
         }
-        }
-    
+        
+        await getBranch();
 
 
-    await getName();
-    await getBranch();
-    const data =fs.readFileSync("hello2.js",'utf-8',(err)=>console.log(err))
-    console.log(data + `\nhe\nllo
-    World!`)
+    // await getName();
+    // const data =fs.readFileSync("hello2.js",'utf-8',(err)=>console.log(err))
+    // console.log(data + `\nhe\nllo
+    // World!`)

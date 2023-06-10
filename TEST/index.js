695062e2f4b795cad533d842f470d9afdbfe5ba3
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-10 10:20:58 +0530

    package installer script and folder struct init

--- TEST/index.js
+++ TEST/index.js
@@ -17,12 +17,13 @@ import {exec} from "child_process";
                     message: 'NodeForce CLI will create file structures on this working folder and install dependencies.Proceed?[y/n]'
                 }
             ])
+
             let choice_of_proceeding;
             choice_of_proceeding = await query1.choice_of_proceeding
 
             if(choice_of_proceeding=='y'){ await fs.writeFileSync("hello3.js", choice_of_proceeding + "Yes",(err=>console.log("err!")));
             
-            var yourscript = exec('sh create-folder-struct.sh',
+            var CREATEFOLDERSCRIPT = exec('sh create-folder-struct.sh',
                 (error, stdout, stderr) => {
                     console.log(stdout);
                     console.log(stderr);
@@ -32,6 +33,41 @@ import {exec} from "child_process";
                 });
             }
             else exit(1);
+            let choice_of_installing_via_nodeforce;
+            const query2= await  inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'choice_of_installing_via_nodeforce',
+                    message: 'Install Dependencies via NodeForce?[y/n]'
+                }
+            ])
+            choice_of_installing_via_nodeforce= await query2.choice_of_installing_via_nodeforce;
+
+            if(choice_of_installing_via_nodeforce=='y'){
+                let writedependencies;
+        
+                const query3= await  inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'writedependencies',
+                        message: 'type packages name with spaces between two : '
+                    }
+                ])
+                writedependencies= query3.writedependencies;
+                await fs.writeFileSync("install-dependencies.sh","npm i " +  writedependencies,(err=>console.log("err!")))
+
+                var RUNDEPENDENCYINSTALLER= exec('sh install-dependencies.sh',
+                (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                    if (error !== null) {
+                        console.log(`exec error: ${error}`);
+                    }
+                });
+            }
+            else{
+                console.log('cool');
+            }
 
 
 
